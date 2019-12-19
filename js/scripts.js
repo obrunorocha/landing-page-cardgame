f83d834858cabb22eb8ed71469c52f60e6147580
Bruno Rocha <obrunorocha@outlook.com>
2019-12-19 20:04:41 -0300

    Alterações de responsividade e ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,81 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        margin:10,
-        autoWidth:false,
-        nav: true,
-        navText:[
-            "<i class='fas fa-chevron-left' aria-hidden='true'></i>",
-            "<i class='fas fa-chevron-right' aria-hidden='true'></i>"
-        ],
-        responsive: {
-            0: {
-              items: 1
-            },
-            768: {
-              items: 2
-            },
-            1200: {
-              items: 3
-            },
-        }
-    });
-})
+//Slide slick
+$('.character-content').slick({
+  dots: false,
+  variableWidth: true,
+  arrows: true,
+  prevArrow: '<img class="slide-arrow prev-arrow" src="images/arrow-prev.png" alt="">',
+  nextArrow: '<img class="slide-arrow next-arrow" src="images/arrow-next.png" alt="">',
+  infinite: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        variableWidth: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+//Formulário de validação
+$("#form").submit(function() {
+  var nome = document.getElementById('nome').value;
+  var email = document.getElementById('email').value;
+  var msg = document.getElementById('msg').value;
+
+  console.log(nome);
+  console.log(email);
+  console.log(msg);
+
+  var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$/;
+  if(regex.test(email)){
+      document.getElementById("resposta").innerHTML = '';
+
+      if(nome != "" && email != "" && msg != ""){
+          document.getElementById("resposta").innerHTML = 'Mensagem enviada com sucesso!';
+          return false;
+      }else{
+          document.getElementById("resposta").innerHTML = '';
+      }
+  }else{
+      document.getElementById("resposta").innerHTML = '';
+  }
+});
+
+//Scroll to top
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  mybutton.style.display = "none";
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    mybutton.style.display = "block";
+  }
+}
